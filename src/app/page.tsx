2b5505987e068689b76df720ab9425c721fde61f
Diego Marin <dmarin@correo.um.edu.uy>
2026-04-28 12:16:25 -0300

    Nombres de fotos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import StatsBar from "@/components/StatsBar";
-import AboutSection from "@/components/AboutSection";
-import ServicesSection from "@/components/ServicesSection";
-import HowItWorks from "@/components/HowItWorks";
-import SittersSection from "@/components/SittersSection";
-import SimulatorSection from "@/components/SimulatorSection";
-import WhatsAppCTA from "@/components/WhatsAppCTA";
-import Footer from "@/components/Footer";
-import FadeIn from "@/components/FadeIn";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import StatsBar from "../components/StatsBar";
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import HowItWorks from "../components/HowItWorks";
+import SittersSection from "../components/SittersSection";
+import SimulatorSection from "../components/SimulatorSection";
+import WhatsAppCTA from "../components/WhatsAppCTA";
+import Footer from "../components/Footer";
+import FadeIn from "../components/FadeIn";
 
 export default function Home() {
   return (
