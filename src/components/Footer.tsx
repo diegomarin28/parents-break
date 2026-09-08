8900a9b1a56d003206e715ebc946c786c6ea9cad
Diego Marin Arnoletti <dmarin@correo.um.edu.uy>
2026-09-08 16:45:09 -0300

    Update WhatsApp URL in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 
 export default function Footer() {
-  const WA_URL = "https://wa.me/59896127376";
+  const WA_URL = "https://wa.me/59897442696";
 
   return (
     <footer className="footer">
